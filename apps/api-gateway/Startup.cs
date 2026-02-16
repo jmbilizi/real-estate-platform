4d675c8aa09c8c8ae4f4d8564cbe10214f2a5b66
Janvier Mbilizi <janviermbilizi@gmail.com>
2026-02-16 01:11:27 -0500

    docs: refine comment on redirecting root URL to Swagger UI

--- apps/api-gateway/Startup.cs
+++ apps/api-gateway/Startup.cs
@@ -92,7 +92,7 @@ namespace ApiGateway
 
             app.UseEndpoints(endpoints =>
             {
-                // Redirect root URL/Request to Swagger UI
+                // Redirect root URL to Swagger UI
                 endpoints.MapGet("/", context =>
                 {
                     context.Response.Redirect("/swagger", permanent: false);
