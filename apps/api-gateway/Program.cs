5dd8784ac53287ad43bd6bc33c0c2ee5acc025c0
Janvier Mbilizi <janviermbilizi@gmail.com>
2026-05-11 23:41:48 -0400

    test(ci): verify .cs change triggers image build

--- apps/api-gateway/Program.cs
+++ apps/api-gateway/Program.cs
@@ -1,6 +1,7 @@
 // <copyright file="Program.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
+// Entry point for the API Gateway.
 
 namespace ApiGateway
 {
